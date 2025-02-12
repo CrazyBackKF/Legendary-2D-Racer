29f01bdcc51e260eab5460549f53a5cd7330b834
CrazyBackKF <devilkf9@gmail.com>
2025-02-12 20:17:27 +0100

    tło

--- classes/player.js
+++ classes/player.js
@@ -10,13 +10,13 @@ class Player {
         this.speed = 0; //zmienna służąca do stopniowej zmiany prędkości
         this.speedMultiplier = 1; //służy do zwiększania prędkości po naciśnięciu turbo
         this.driftMultiplier = 1;
-        this.speedValue = 0.015;    //zmienna dodajaca predkosc z kazda klatka z wcisnietym klawiszem [w / s]
-        this.friction = 0.008;  //zmienna opisujaca tarcie[o ile hamuje bez kliknietych klawiszy]
+        this.speedValue = 0.03;    //zmienna dodajaca predkosc z kazda klatka z wcisnietym klawiszem [w / s]
+        this.friction = 0.01;  //zmienna opisujaca tarcie[o ile hamuje bez kliknietych klawiszy]
         this.maxSpeed = 3; //maksymalna prędkość z jaką może jechać pojazd
         this.turboAmount = 5; //maksymalna ilość turbo
         this.lastTurbo = 0; //ostatnie kliknięcie turbo
-        this.image = new Image();
-        this.image.src = "img/sport_green.png";
+        //this.image = new Image();
+        //this.image.src = "img/sport_green.png";
         //prędkość w poziomie x i y
         this.velocity = {
             x: 0,
@@ -78,12 +78,12 @@ class Player {
     // Metoda która wyświetla pojazd
     draw() {
         c.save();
-        c.translate(player.position.x + player.width / 2, player.position.y + player.height / 4);
+        c.translate(this.position.x + this.width / 2, this.position.y + this.height / 4);
         c.rotate(convertToRadians(this.angle));
 
-        c.drawImage(this.image, -player.width / 2, -player.height / 4,);
-        c.fillStyle = "rgba(255, 0, 0, 0.5)";
-        c.fillRect(-player.width / 2, -player.height / 4, this.image.width, this.image.height);
+        //c.drawImage(this.image, -this.width / 2, -this.height / 4,);
+        c.fillStyle = "rgba(255, 0, 0, 1)";
+        c.fillRect(-this.width / 2, -this.height / 4, this.width, this.height);
 
         c.restore();
     }
